b878e75819311b94dc12865cc8dc82ede0774c0b
gexgener1l <77413669+gexgener1l@users.noreply.github.com>
2024-02-19 19:52:23 +0300

    fixes 2.0

--- src/main/java/com/webblog/blog/serveces/CheckerService.java
+++ src/main/java/com/webblog/blog/serveces/CheckerService.java
@@ -1,18 +1,27 @@
 package com.webblog.blog.serveces;
+
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+
 public class CheckerService {
+
+    // Private constructor to hide the implicit public one
+    private CheckerService() {
+        // Private constructor implementation (if needed)
+    }
+
     public static String checkSite(String url) {
         try {
             Connection.Response response = Jsoup.connect(url).execute();
             int statusCode = response.statusCode();
-
             Document document = response.parse();
             String pageTitle = document.title();
             String content = "content";
+
             // Мета-теги
             Elements metaTags = document.select("meta");
             String metaDescription = metaTags.select("meta[name=description]").attr(content);
@@ -22,11 +31,12 @@ public class CheckerService {
             String lastModified = document.select("meta[name=last-modified]").attr(content);
 
             return "{ \"status\": \"success\", \"message\": \"Сайт " + url + " проверен\", \"pageTitle\": \"" + pageTitle + "\", \"statusCode\": " + statusCode + ", \"lastModified\": \"" + lastModified + "\", \"metaDescription\": \"" + metaDescription + "\", \"metaKeywords\": \"" + metaKeywords + "\" }";
-
-        } catch (Exception e) {
-            return "{ \"status\": \"error\", \"message\": \"Ошибка при проверке сайта " + url + "\", \"errorDetails\": \"" + e.getMessage() + "\" }";
+        } catch (IOException e) {
+            return createErrorMessage(url, e);
         }
     }
 
-
+    private static String createErrorMessage(String url, Exception e) {
+        return "{ \"status\": \"error\", \"message\": \"Ошибка при проверке сайта " + url + "\", \"errorDetails\": \"" + e.getMessage() + "\" }";
+    }
 }
