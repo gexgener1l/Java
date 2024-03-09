61f9df0a4ec5c190027c294617c7717a4cd9b9d8
gexgener1l <77413669+gexgener1l@users.noreply.github.com>
2024-03-09 20:53:07 +0300

    fixes

--- src/main/java/com/webblog/blog/controllers/WebPostController.java
+++ src/main/java/com/webblog/blog/controllers/WebPostController.java
@@ -1,14 +1,14 @@
 package com.webblog.blog.controllers;
 
 import com.webblog.blog.model.WebPost;
-import com.webblog.blog.repository.WebPostRepository;
 import com.webblog.blog.serveces.WebPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Date;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
@@ -22,12 +22,12 @@ public class WebPostController {
     }
 
     @PostMapping("/postBlog")
-    public ResponseEntity<WebPost> createWebPost(WebPost webPost) {
+    public ResponseEntity<WebPost> createWebPost(@RequestBody WebPost webPost) {
         try {
             WebPost createdWebPost = webPostService.createWebPost(webPost);
             return new ResponseEntity<>(createdWebPost, HttpStatus.CREATED);
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
